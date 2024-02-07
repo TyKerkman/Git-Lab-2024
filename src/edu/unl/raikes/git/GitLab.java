ac6053e7b58d6f1735618bc48c7ec9b010154f08
Minimanminh <mvu10@huskers.unl.edu>
2024-02-07 11:20:55 -0600

    Commit 2

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -4,7 +4,8 @@ public class GitLab {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("This is a lesson on git.");
+        GitLabHelper helper = new GitLabHelper("Tyler", "Minh");
+        helper.PrintWelcomeMessage();
     }
 
 }
